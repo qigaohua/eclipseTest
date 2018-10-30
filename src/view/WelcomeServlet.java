927e714d441527951a174fdd5ad06f6bcbc8ae65
sinpo828 <jiaqianyiren@126.com>
2018-10-30 16:07:17 +0800

    修改输出信息

--- src/view/WelcomeServlet.java
+++ src/view/WelcomeServlet.java
@@ -28,7 +28,7 @@ public class WelcomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.getWriter().print("Hello Test");
+		response.getWriter().print("eclipse github test");
 	}
 
 	/**
